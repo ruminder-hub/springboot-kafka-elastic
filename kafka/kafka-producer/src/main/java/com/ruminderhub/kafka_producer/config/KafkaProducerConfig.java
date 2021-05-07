51d32ae015350a58be1b4ccffb4cf3c28d662604
Ruminder Singh <ruminder.inbox@gmail.com>
2021-05-06 21:56:48 -0300

    Kafka Producer configured

--- kafka/kafka-producer/src/main/java/com/ruminderhub/kafka_producer/config/KafkaProducerConfig.java
+++ kafka/kafka-producer/src/main/java/com/ruminderhub/kafka_producer/config/KafkaProducerConfig.java
@@ -1,10 +1,15 @@
 package com.ruminderhub.kafka_producer.config;
 
+import com.fasterxml.jackson.databind.ser.std.StringSerializer;
 import com.ruminderhub.twitter_kafka.config.KafkaConfigData;
 import com.ruminderhub.twitter_kafka.config.KafkaProducerConfigData;
 import org.apache.avro.specific.SpecificRecordBase;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
 
 import java.io.Serializable;
 import java.util.HashMap;
@@ -24,6 +29,26 @@ public class KafkaProducerConfig<K extends Serializable, V extends SpecificRecor
     @Bean
     public Map<String, Object> producerConfig() {
         Map<String, Object> props = new HashMap<>();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaConfigData.getBootstrapServers());
+        props.put(kafkaConfigData.getSchemaRegistryUrlKey(), kafkaConfigData.getSchemaRegistryUrl());
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, kafkaProducerConfigData.getKeySerializerClass());
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, kafkaProducerConfigData.getValueSerializerClass());
+        props.put(ProducerConfig.BATCH_SIZE_CONFIG, kafkaProducerConfigData.getBatchSize());
+        props.put(ProducerConfig.REQUEST_TIMEOUT_MS_CONFIG, kafkaProducerConfigData.getRequestTimeoutMs());
+        props.put(ProducerConfig.LINGER_MS_CONFIG, kafkaProducerConfigData.getLingerMs());
+        props.put(ProducerConfig.ACKS_CONFIG, kafkaProducerConfigData.getAcks());
+        props.put(ProducerConfig.COMPRESSION_TYPE_CONFIG, kafkaProducerConfigData.getCompressionType());
+        props.put(ProducerConfig.RETRIES_CONFIG, kafkaProducerConfigData.getMaxRetryCount());
         return props;
+    }
+
+    @Bean
+    public ProducerFactory producerFactory() {
+        return new DefaultKafkaProducerFactory(producerConfig());
+    }
+
+    @Bean
+    public KafkaTemplate<K, V> kafkaTemplate() {
+
     }
 }
