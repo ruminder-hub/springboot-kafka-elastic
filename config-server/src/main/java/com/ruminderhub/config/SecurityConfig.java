24617bcb3b772f9c6f5f2a734cc03856a9f712e4
Ruminder Singh <ruminder.inbox@gmail.com>
2021-05-24 20:51:59 -0300

    Config server added to the docker

--- config-server/src/main/java/com/ruminderhub/config/SecurityConfig.java
+++ config-server/src/main/java/com/ruminderhub/config/SecurityConfig.java
@@ -1,2 +1,17 @@
-package com.ruminderhub.config;public class SecurityConfig {
+package com.ruminderhub.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/encrypt/**").antMatchers("/decrypt/**");
+        super.configure(web);
+    }
+
 }
